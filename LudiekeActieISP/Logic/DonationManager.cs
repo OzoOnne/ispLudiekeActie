98f483451d854af663ece718c7d5b9230b130583
RickvdAkkerWH <s1193906@student.windesheim.nl>
2026-01-02 11:13:19 +0100

    did some stuff

--- LudiekeActieISP/Logic/DonationManager.cs
+++ LudiekeActieISP/Logic/DonationManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,24 @@ namespace Logic
 {
     public class DonationManager
     {
+        private decimal _dayDonations;
+        private decimal _kilometers;
 
         public decimal GetTotalDonations()
         {
-            return GetSingleDonations() + GetPerKilometerDonations();
+            return _dayDonations + GetSingleDonations() + GetPerKilometerDonations();
         }
         public decimal GetPerKilometerDonations()
         {
             return Random.Shared.Next(1, 10);
+        }
+        public void SetDayDonations(decimal amount)
+        { 
+            _dayDonations = amount;
+        }
+        public void SetKilometers(decimal kilometers)
+        {
+            _kilometers = kilometers;
         }
         private decimal GetSingleDonations()
         {
---
+++ LudiekeActieISP/Logic/DonationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class DonationModel
+    {
+        public bool IsPerKilometerDonation { get; set; }
+        public decimal PerKilometerAmount { get; set; }
+        public decimal MaxAmount { get; set; }
+        public decimal SingleDonationAmount { get; set; }
+    }
+}
