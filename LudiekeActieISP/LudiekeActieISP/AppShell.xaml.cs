bc7323b006aae34c1d39d38dafa91c1be0efc011
OzoOnne <55187541+OzoOnne@users.noreply.github.com>
2026-01-02 10:10:40 +0100

    abc

---
+++ LudiekeActieISP/LudiekeActieISP/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LudiekeActieISP
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LudiekeActieISP/LudiekeActieISP/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LudiekeActieISP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
