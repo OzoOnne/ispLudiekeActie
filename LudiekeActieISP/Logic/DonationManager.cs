e4d6a52b097a8a2200eefb94298c29ff46e5dcbf
RickvdAkkerWH <s1193906@student.windesheim.nl>
2026-01-02 10:28:58 +0100

    donation logic

---
+++ LudiekeActieISP/Logic/DonationManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public class DonationManager
+    {
+
+        public decimal GetTotalDonations()
+        {
+            return Random.Shared.Next(1000, 10000);
+        }
+        public decimal GetPerKilometerDonations()
+        {
+            return Random.Shared.Next(1, 10);
+        }
+    }
+}
